498e7747ed6f807a4efd2d49c661151316092a7b
cudjoeab <cudjoeab@gmail.com>
2019-08-06 16:07:37 -0400

    adding a user foreign key to Picture model

--- photogur/models.py
+++ photogur/models.py
@@ -1,9 +1,11 @@
 from django.db import models 
+from django.contrib.auth.models import User 
 
 class Picture(models.Model):
     title = models.CharField(max_length=255)
     artist = models.CharField(max_length=255)
     url = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete= models.CASCADE, related_name= 'user', null=True)
 
     def __str__(self):
         return self.title 

65b8cfcaad141e50fd02543474e5a30325cbe974
cudjoeab <cudjoeab@gmail.com>
2020-01-11 13:51:22 -0500

    initialize Django project

--- photogur/settings.py
+++ photogur/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -118,7 +119,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
+
+django_heroku.settings(locals())
